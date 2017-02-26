7f436d03e516444fc75db2eb831f1d1c5b6cb301
simonepri <simone.pri@hotmail.it>
2017-02-26 21:14:34 +0100

    Fix repo link

--- ardutank/ardutank.ino
+++ ardutank/ardutank.ino
@@ -2,7 +2,7 @@
  * Ardutank
  *
  * @author        Simone Primarosa,(http://simoneprimarosa.com)
- * @link          (https://github.com/simonepri/ardu-tank)
+ * @link          (https://github.com/simonepri/ardutank)
  * @license       MIT License (https://opensource.org/licenses/MIT)
  */
 
